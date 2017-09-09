0347c2cbdee80f1682bc98e2b4e88ce7f1671d06
jakad <jakadamko@gmail.com>
2017-09-09 16:01:38 -0400

    updated 9/9/2017

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,131 @@
-var randomWordArr = ["beer", "wine", "vodka", "rum", "gin"];
-var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+$(document).ready(function() {
 
-console.log(randomWord);
+    var listOfWords = ["coorslight", "budlight", "bluemoon", "heineken", "millerlite"];
+    var wrdToBeGuessed = listOfWords[Math.floor(Math.random() * listOfWords.length)];
+    var rightGuess = [];
+    var rightGuessMatch = [];
+    var wrongGuess = [];
+    var CharLength = wrdToBeGuessed.length;
+    var lives = 20;
 
-var s;
-var r;
-var count = 0;
-//Empty array to store the guesses
-var answerArray = [];
-var wAnswerArray = [];
+    console.log("secret word = " + wrdToBeGuessed);
 
-//fill the answer array with under scores which isn't working for some reason
-function startUp() {
-    for (var i = 0; i < randomWord.length; i++) {
-        answerArray[i] = "_";
+    $("#Box4").append(CharLength + " Letter word | Category: Beer Brands");
+    //document.getElementById("RSbutton").onclick = window.location.reload()
+    function reloadPage() {
+        location.reload();
     }
-    //Put them in a string
-    s = answerArray.join(" ");
-    document.getElementById("answer").innerHTML = s;
-}
 
 
+    for (var i = 0; i < wrdToBeGuessed.length; i++) {
+        rightGuess.push([]);
+    }
 
 
+    for (var i = 0; i < wrdToBeGuessed.length; i++) {
+        rightGuessMatch.push(wrdToBeGuessed.charAt(i));
+    }
+
+    document.getElementById("Box5").innerHTML = "Guesses Remaining: " + lives;
+    document.getElementById("Box3").innerHTML = "The Secret Word: " + rightGuess;
 
 
+    function keyPress(event) {
 
-function Letter() {
+        var userGuess = event.key;
+        document.getElementById("Box2").innerHTML = "Your Guesses: " + userGuess;
+        lives -= 1;
+        document.getElementById("Box5").innerHTML = "Guesses Remaining: " + lives;
 
-    //we get the letter the user typed in the inputbox
-    var letter = document.getElementById("letter").value;
+        for (var i = 0; i < wrdToBeGuessed.length; i++) {
 
-    //makes sure we have a guess more checks can be made here, only letters
-    if (letter.length > 0) {
-        for (var i = 0; i < randomWord.length; i++) {
-
-            //now if the random word contains a letter that the user typed in
-            if (randomWord[i] === letter) {
-
-                //assin it to the letter
-                answerArray[i] = letter;
+            if (userGuess == rightGuess[i]) {
+                //Do Nothing - Makes sure double entry cannot be made
             } else {
-                wAnswerArray[i] = letter;
 
+                var PwLetter = wrdToBeGuessed.charAt(i);
+
+                if (PwLetter == userGuess) {
+
+                    rightGuess[i].push(userGuess);
+
+                    document.getElementById("Box3").innerHTML = "The Secret Word: " + rightGuess;
+                }
             }
         }
-        count++;
-        document.getElementById("counter").innerHTML = "No of attempts: " + count;
-        document.getElementById("answer").innerHTML = answerArray.join(" ");
+
+        console.log("wrongGuess = " + wrongGuess.join());
+        console.log("rightGuess = " + rightGuess.join());
+        wrongGuess.push(userGuess);
+        document.getElementById("Box2").innerHTML = "Your Guesses: " + wrongGuess;
+
+        if (wrongGuess.length > 15) {
+            alert("sorry, you lost");
+            //window.location.reload();
+        }
+
+        console.log("wronglength" + wrongGuess.length);
+
+
+        if (rightGuess.join() == rightGuessMatch.join()) {
+            alert("Congrats! You Win!");
+            //window.location.reload();
+            //document.getElementById("Box6").src = "coors.JPG";
+
+
+            if (rightGuess == "C,O,O,R,S,L,I,G,H,T") {
+                var img = document.createElement("img");
+                img.src = "assets/images/coors.JPG";
+
+                var src = document.getElementById("Box6");
+                src.appendChild(img);
+
+            }
+
+
+            if (rightGuess == "h,e,i,n,e,k,e,n") {
+                var img = document.createElement("img");
+                img.src = "assets/images/heineken.JPG";
+
+                var src = document.getElementById("Box6");
+                src.appendChild(img);
+
+            }
+
+            if (rightGuess == "m,i,l,l,e,r,l,i,t,e") {
+                var img = document.createElement("img");
+                img.src = "assets/images/millerlite.JPG";
+
+                var src = document.getElementById("Box6");
+                src.appendChild(img);
+
+            }
+
+            if (rightGuess == "b,l,u,e,m,o,o,n") {
+                var img = document.createElement("img");
+                img.src = "assets/images/bluemoon.JPG";
+
+                var src = document.getElementById("Box6");
+                src.appendChild(img);
+
+            }
+
+            if (rightGuess == "b,u,d,l,i,g,h,t") {
+                var img = document.createElement("img");
+                img.src = "assets/images/budlight.JPG";
+
+                var src = document.getElementById("Box6");
+                src.appendChild(img);
+
+            }
+
+
+
+        }
     }
-    if (count > 5) {
-        document.getElementById("stat").innerHTML = "Come on - you should have guessed it by now";
-    }
-}
+
+
+
+
+    document.addEventListener('keydown', keyPress);
+});
